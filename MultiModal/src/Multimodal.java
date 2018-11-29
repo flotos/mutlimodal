6557b03a709e695878e675bb03a8972f51fd8da2
Florian Peyron <flotos@hotmail.fr>
2018-11-29 11:12:02 +0100

    basic not working ivy app

---
+++ MultiModal/src/Multimodal.java
@@ -0,0 +1,10 @@
+import fr.dgac.ivy.Ivy;
+
+public class Multimodal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ivy qsdf = new Ivy("testAgent", "monMessage", appcb);
+	}
+
+}
